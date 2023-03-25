ffe7559f37aa8d6d0bb93c21f5c30e338875a0c2
Pete0729 <79485508+Pete0729@users.noreply.github.com>
2023-03-25 15:49:47 -0500

    basic landing page

--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,1 +1,21 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
 
+const inter = Inter({ subsets: ['cyrillic'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>No Unknown</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <main className={styles.main}>
+       <h1>No Unknown</h1>
+       
+      </main>
+    </>
+  )
+}
